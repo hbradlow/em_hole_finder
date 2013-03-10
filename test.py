baf2f90b2efd55fce239793f9930291f02cd00e9
Henry Bradlow <hbradlow@berkeley.edu>
2013-03-10 12:44:09 -0700

    Added test file as an example

---
+++ test.py
@@ -0,0 +1,21 @@
+from find_mask import Pipeline
+
+filename = "test_images/test1.jpg"
+p = Pipeline(downsample=20,filename=filename)
+
+p.open() #perform background subtraction
+p.blur() #blur the image
+p.crop(factor=15) #crop out the black borders that are produced by the blur
+p.threshold() #do adaptive thresholding
+p.erode() #erode the thresholded image
+
+p.connected_components_iterative() #calculate the connected components
+p.select_largest_component() #select the largest connected component
+
+p.connected_components_iterative() #calculate the connected components of the inverted image
+p.select_largest_component() #selected the largest connected components of the inverted image (this should now be the interior of the hole)
+
+p.dilate() #dilate the mask
+p.mask_original() #mask the original image with the calculated mask
+p.show() #show the mask for debugging
+p.save_to_file() #saved the masked image to a file
