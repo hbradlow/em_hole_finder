0556330d9320a70b9dd81bcd7b9502b16ecf0aec
Henry Bradlow <hbradlow@berkeley.edu>
2013-03-10 12:27:17 -0700

    Refactored a lot

--- utils.py
+++
@@ -1,7 +0,0 @@
-def ipython_on_error():
-    import sys
-    import IPython
-    def excepthook(type, value, traceback):
-        print value
-        IPython.embed()
-    sys.excepthook = excepthook
