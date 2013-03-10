b5e74d618c4330aa5f26ec4b410f27de4c640b3b
Henry Bradlow <hbradlow@berkeley.edu>
2013-03-10 11:57:36 -0700

    Whole thing basically working

--- find_circle.py
+++ find_circle.py
@@ -14,16 +14,18 @@ from skimage import data, io, filter
 from skimage import data
 from skimage.filter import threshold_otsu
 from skimage.segmentation import clear_border
-from skimage.morphology import label, closing, square, reconstruction
+from skimage.morphology import label, closing, square, reconstruction, erosion
 from skimage.measure import regionprops
 import skimage.draw
+
+from scipy.ndimage.morphology import grey_opening
 
 import IPython
 
 HOLE_RADIUS = 250
 
 class Pipeline:
-    def __init__(self,filename="data/test4.jpg",debug=True,downsample=20):
+    def __init__(self,filename="data/test2.jpg",debug=True,downsample=20):
         self.image = Image.open(filename)
         self.data = np.asarray(self.image)[:,:]
 
@@ -33,6 +35,11 @@ class Pipeline:
 
         self.debug = debug
         self.point_inside_penalty = 50000000
+
+    def open(self,window_size=(10,10)):
+       self.data = np.max(self.data) - self.data
+       self.data -= grey_opening(self.data,size=(5,5)) 
+       self.data = np.max(self.data) - self.data
 
     def select_largest_component(self):
         max_component = max(self.component_sizes.items(),key=lambda x: x[1])[0]
@@ -50,10 +57,60 @@ class Pipeline:
                 self.data[x,y] = 0
 
     def fill_holes(self):
-        mask = np.zeros(self.data.shape)
+        from skimage.morphology import square
+        self.data = np.max(self.data)-self.data
+        self.data = erosion(self.data,square(3))
+        self.data = np.max(self.data)-self.data
+        """
+        mask = np.ones(self.data.shape)*np.min(self.data)
         seed = self.data
 
-        self.data = reconstruction(seed, mask, method='dilation')
+        self.data = reconstruction(seed, mask, method='erosion')
+        """
+
+    def connected_components_iterative(self):
+        component_cache = np.zeros(self.data.shape)
+        self.component_sizes = {}
+        num_components = 0
+
+        def get_neighbors(x,y,depth=1):
+            for i in range(-depth,depth+1):
+                for j in range(-depth,depth+1):
+                    if i+x<self.data.shape[0] and j+y<self.data.shape[1] and i+x>=0 and j+y>=0 and (i != 0 or j != 0):
+                        yield (i+x,j+y)
+
+        self.num_components = 0
+
+        pixel_stack = []
+        for (x,y),value in np.ndenumerate(self.data):
+            if not value:
+                pixel_stack.append((x,y))
+        while pixel_stack:
+            x,y = pixel_stack.pop()
+            if component_cache[x,y]!=0 or self.data[x,y]:
+                continue
+
+            found_component = 0
+
+            for (nx,ny) in get_neighbors(x,y):
+                if component_cache[nx,ny] != 0:
+                    component_cache[x,y] = component_cache[nx,ny]
+                    found_component = component_cache[nx,ny]
+                    self.component_sizes[found_component] += 1
+                    break
+
+            if not found_component:
+                self.num_components += 1
+                found_component = self.num_components
+                component_cache[x,y] = found_component
+                self.component_sizes[found_component] = 1
+
+            for (nx,ny) in get_neighbors(x,y):
+                pixel_stack.append((nx,ny))
+
+        self.data = component_cache
+        print self.component_sizes
+
 
     def connected_components(self):
         component_cache = np.zeros(self.data.shape)
@@ -83,7 +140,7 @@ class Pipeline:
 
             if not found_component:
                 self.num_components += 1
-                found_component = self.num_components
+                found_component = random.randrange(255)+1 #self.num_components
                 component_cache[x,y] = found_component
                 self.component_sizes[found_component] = 1
 
@@ -120,14 +177,42 @@ class Pipeline:
     def blur(self,window_size=(10,10)):
         window = np.ones(window_size)
         window /= np.linalg.norm(window)
-        self.data = scipy.signal.convolve2d(self.data, window)
+        self.data = scipy.signal.convolve2d(self.data, window)#, boundary='fill',fillvalue=np.max(self.data))
+
+    def fit_plane(self):
+        X,Y = np.mgrid[:self.data.shape[0],:self.data.shape[1]]
+        x = X.flatten()
+        y = Y.flatten()
+        A = np.column_stack((np.ones(x.size), x, y))
+        c, resid,rank,sigma = np.linalg.lstsq(A,self.data.flatten())
+        self.c = c
 
     def plot_3d(self):
         fig = plt.figure()
-        ax = fig.add_subplot(1,1,1, projection='3d')
+
         X,Y = np.mgrid[:self.data.shape[0],:self.data.shape[1]]
+
+        normal = np.array([self.c[2],self.c[1],1])
+
+        z = (-normal[0]*X - normal[1]*Y + self.c[0])/normal[2]
+
+        print "HERE"
+        ax = fig.add_subplot(1,1,1, projection='3d')
         surf = ax.plot_surface(X,Y,self.data)
+        surf = ax.plot_surface(X,Y,z)
+        print "HERE"
+
+        a = np.average(z)
+        for (x,y),value in np.ndenumerate(z):
+            self.data[x,y] += a-value
+        print "HERE"
+
+        ax2 = fig.add_subplot(1,1,1, projection='3d')
+        surf = ax2.plot_surface(X,Y,self.data)
+        surf = ax2.plot_surface(X,Y,z)
         plt.show()
+
+
 
     def dilate(self):
         def get_neighbors(x,y,depth=1):
@@ -157,7 +242,7 @@ class Pipeline:
                 else:
                     self.data[i,j] = np.max(self.data)
         """
-        self.data = filter.threshold_adaptive(self.data,80,offset=0)
+        self.data = filter.threshold_adaptive(self.data,100)
 
     def connectivity_threshold(self,threshold=24,depth=2):
         """
@@ -182,8 +267,8 @@ class Pipeline:
         self.data = self.data[factor:-factor,factor:-factor]
 
     def decimate(self,downsample=20):
-        self.data = scipy.signal.decimate(self.data,downsample,axis=0)
-        self.data = scipy.signal.decimate(self.data,downsample,axis=1)
+        self.data = scipy.signal.decimate(self.data,downsample,axis=0,ftype='fir')
+        self.data = scipy.signal.decimate(self.data,downsample,axis=1,ftype='fir')
 
     def movingaverage(self, window_size=5):
         window = np.ones(window_size)
@@ -196,6 +281,10 @@ class Pipeline:
         self.data = self.data - np.ones(self.data.shape)*image_min
         self.data *= (image_max)/(image_max-image_min)
 
+    def save_to_file(self):
+        rescaled = (255.0 / self.data.max() * (self.data - self.data.min())).astype(np.uint8)
+        im = Image.fromarray(rescaled)
+        im.save("output.jpg")
     def cost(self):
         def c(beta):
             def in_circle(x,y):
@@ -216,20 +305,32 @@ class Pipeline:
         return c
 
 p = Pipeline(downsample=20)
-#p.plot_3d()
+
+p.open()
 p.blur()
+#p.show()
+
+#p.fit_plane()
+#p.plot_3d()
+
 p.crop(factor=15)
-p.show()
 p.threshold()
-p.connected_components()
+
+#p.show()
+p.fill_holes()
+#p.show()
+
+p.connected_components_iterative()
+#p.show()
+
 p.select_largest_component()
-p.connected_components()
+#p.show()
+
+p.connected_components_iterative()
 p.select_largest_component()
-#p.connectivity_threshold()
-#p.fit_circle()
-#IPython.embed()
-#p.fill_holes()
+
 for i in range(10):
     p.dilate()
 p.mask_original()
 p.show()
+p.save_to_file()
